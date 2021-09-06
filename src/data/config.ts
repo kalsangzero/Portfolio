84e3b04756dc90cd44caef11f3d4e10375f7be00
kalsangzero <kaltse000@gmail.com>
2021-09-06 15:54:06 +0200

    yeah

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,7 +1,7 @@
 const config = {
-  defaultTitle: 'John Doe',
+  defaultTitle: 'Kalsang Tsering',
   url: process.env.NODE_ENV !== 'development' ? process.env.NEXT_PUBLIC_PORTFOLIO_URL : 'http://localhost:3040',
-  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  defaultDescription: 'I’m Kalsang and I’m a Backend & Devops engineer!',
   googleAnalyticsID: 'G-PE8QW0ZGTZ',
 };
 
